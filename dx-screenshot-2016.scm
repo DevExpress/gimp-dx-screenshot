4178aa1f837b579c56d47ed47f1057e19d146c82
glagolev.vladislav <glagolev.vladislav@1e62e12a-7c42-0249-8c77-3915865c2936>
2016-07-19 15:53:34 +0000

    WIP
    git-svn-id: file:///D/SVN/script-fu@74 1e62e12a-7c42-0249-8c77-3915865c2936

--- dx-screenshot-2016.scm
+++ dx-screenshot-2016.scm
@@ -9,8 +9,9 @@
                                     draw-border
                                     border-color
                                     border-opacity
-)
-
+                                    wavy-crop
+                                    amplitude
+                                    reverse-phase)
 (let* (
         (image-width (car (gimp-image-width image)))
         (image-height (car (gimp-image-height image)))
@@ -29,17 +30,17 @@
         (bordered-selection 0)
         (user-selection 0)
 
-        (sel-docked-left FALSE)
-        (sel-docked-right FALSE)
-        (sel-docked-top FALSE)
-        (sel-docked-bottom FALSE)
-    )
+        (sel-docked-left FALSE) (sel-docked-right FALSE)
+        (sel-docked-top FALSE) (sel-docked-bottom FALSE)
+        (num-of-docked 0)
 
-  (gimp-context-push)
+        (x1 0) (y1 0) (x2 0) (y2 0) (x 0) (y 0) (phase 0) (points 0) (point 0)
+
+    )
+(gimp-context-push)
 ;BEGIN -----------------------------------------------------------
     (if (= is-GIF TRUE) (gimp-image-convert-rgb image)) ; No opacity in INDEXED
     (gimp-image-set-active-layer image background-layer) ; The main layer
-    (set! user-selection (car (gimp-selection-save image))) ; Save selection
 
     (if (= draw-border TRUE) (begin           ; --------- Border Start ---------
     (gimp-image-undo-group-start image)
@@ -51,19 +52,28 @@
             (set! image-width (+ image-width 2))   ; Update image
             (set! image-height (+ image-height 2)) ; demendions
 
+            (set! wavy-crop FALSE) ; Disable wavy-cropper
+
             (gimp-selection-all image)
         ) (begin  ; Else (selection exists)
+            (set! user-selection (car (gimp-selection-save image))) ; Save it
+
             ; Check margins for border
             (if (= (list-ref (gimp-selection-bounds image) 1) 0)
                 (set! sel-docked-left TRUE)
+                (set! num-of-docked (+ num-of-docked 1))
             ) (if (= (list-ref (gimp-selection-bounds image) 2) 0)
                 (set! sel-docked-top TRUE)
+                (set! num-of-docked (+ num-of-docked 1))
             ) (if (= (list-ref (gimp-selection-bounds image) 3) image-width)
                 (set! sel-docked-right TRUE)
+                (set! num-of-docked (+ num-of-docked 1))
             ) (if (= (list-ref (gimp-selection-bounds image) 4) image-height)
                 (set! sel-docked-bottom TRUE)
+                (set! num-of-docked (+ num-of-docked 1))
             )
-            ; Add required margins for border
+
+            ; Add required margins for border (not optimal, yet self-commented)
             (if (= sel-docked-left TRUE)
                 (gimp-image-resize  image
                                     (+ (car (gimp-image-width  image)) 1)
@@ -100,18 +110,12 @@
 
         (set! bordered-selection (car (gimp-selection-save image)))
 
-        (set! border-layer (car (gimp-layer-new image
-                                                image-width
-                                                image-height
-                                                RGBA-IMAGE
-                                                "Border"
-                                                border-opacity
-                                                NORMAL-MODE
-        )                  )    )
+        (set! border-layer (car (gimp-layer-new image image-width image-height
+                                                RGBA-IMAGE "Border"
+                                                border-opacity NORMAL-MODE)))
         (gimp-drawable-fill border-layer TRANSPARENT-FILL)
         (gimp-image-insert-layer image border-layer 0 ; no parent
                 (- (car (gimp-image-get-layers image)) 1)) ; The pre-last layer
-        (gimp-context-set-foreground border-color)
 
         (gimp-image-select-rectangle image CHANNEL-OP-SUBTRACT
                         (+ (list-ref (gimp-selection-bounds image) 1) 1)  ;x0
@@ -122,31 +126,99 @@
                             (list-ref (gimp-selection-bounds image) 2) 2) ;h
         ) ; (gimp-selection-border sucks)
 
-        (gimp-edit-bucket-fill-full border-layer
-                                    FG-BUCKET-FILL
-                                    NORMAL-MODE
+        (gimp-context-set-foreground border-color)
+        (gimp-edit-bucket-fill-full border-layer FG-BUCKET-FILL NORMAL-MODE
                                     100   ; opacity
                                     0     ; threshold
                                     FALSE ; sample-merged
                                     TRUE  ; fill-transparent
-                                    SELECT-CRITERION-COMPOSITE
-                                    0 0
-        )
+                                    SELECT-CRITERION-COMPOSITE 0 0)
         (gimp-selection-load bordered-selection) ; Load border selection
         (gimp-image-set-active-layer image background-layer)
     (gimp-image-undo-group-end image)
     ))                                         ; --------- Border End ---------
 
+    (if (= wavy-crop TRUE) (begin          ; --------- Wavy crop Start ---------
+        (if (< 2 num-of-docked) (gimp-message "Wawy crop from 3 or more sides is not recommended!"))
+        (set! x1 (car (cdr (gimp-selection-bounds image))))
+        (set! y1 (car (cdr (cdr (gimp-selection-bounds image)))))
+        (set! x2 (car (cdr (cdr (cdr (gimp-selection-bounds image))))))
+        (set! y2 (car (cdr (cdr (cdr (cdr (gimp-selection-bounds image)))))))
+        (gimp-selection-none image) ;remove selection if one exists
 
-    (gimp-image-crop image ; Crop to selection
-                     (- (list-ref (cdr (gimp-selection-bounds image)) 2)
-                        (list-ref (cdr (gimp-selection-bounds image)) 0))
-                     (- (list-ref (cdr (gimp-selection-bounds image)) 3)
-                        (list-ref (cdr (gimp-selection-bounds image)) 1))
-                     (list-ref (cdr (gimp-selection-bounds image)) 0)
-                     (list-ref (cdr (gimp-selection-bounds image)) 1))
+        (set! points (cons-array (* (+ (* 2 (- x2 x1)) (* 2 (- y2 y1))) 2) 'double)) ; amount of points for points array
 
-    (gimp-selection-load user-selection) ; Recover selection
+        (set! x x1)
+        (set! y y1)
+        (set! amplitude (/ amplitude 200))
+        (if (= reverse-phase TRUE) (set! phase 3.1415))
+        ; moving from top-left to top-right
+        (while (< x x2)
+            (aset points point x) ;x
+            (set! point (+ point 1))
+            (if (> y1 0)
+                (aset points point (+ y1 (* (* (- x2 x1) amplitude ) (sin (+ phase (* 6.2832 (/ (- x x1) (- x2 x1)))))))) ;y
+            )
+            (if (<= y1 0)
+                (aset points point 0) ;y
+            )
+            (set! point (+ point 1))
+            (set! x (+ x 1))
+        )
+        ; moving from top-right to bottom-right
+        (while (< y y2)
+            (if (< x2 image-width)
+                (aset points point (+ x2 (* (* (- y2 y1) amplitude) (sin (+ phase (* -6.2832 (/ (- y y1) (- y2 y1)))))))) ;x
+            )
+            (if (>= x2 image-width)
+                (aset points point image-width) ;x
+            )
+            (set! point (+ point 1))
+            (aset points point y) ;y
+            (set! point (+ point 1))
+            (set! y (+ y 1))
+        )
+        ; moving from bottom-right to bottom-left
+        (while (> x x1)
+            (aset points point x) ;x
+            (set! point (+ point 1))
+            (if (< y2 image-height)
+                (aset points point (+ y2 (* (* (- x2 x1) amplitude) (sin (+ phase (* 6.2832 (/ (- x x1) (- x2 x1)))))))) ;y
+            )
+            (if (>= y2 image-height)
+                (aset points point image-height)
+            )
+            (set! point (+ point 1))
+            (set! x (- x 1))
+        )
+        ; moving from bottom-left to top-right
+        (while (> y y1)
+            (if (> x1 0)
+                (aset points point (+ x1 (* (* (- y2 y1) amplitude) (sin (+ phase (* -6.2832 (/ (- y y1) (- y2 y1))))))))
+            )
+            (if (<= x1 0)
+                (aset points point 0)
+            )
+            (set! point (+ point 1))
+            (aset points point y)
+            (set! point (+ point 1))
+            (set! y (- y 1))
+        )
+        ; Actual cropping
+        (gimp-image-select-polygon image CHANNEL-OP-ADD point points)
+        (gimp-selection-invert image)
+        (gimp-edit-clear drawable)
+    ) (begin ; Else (= wavy-crop FALSE)
+        (gimp-image-crop image ; Crop to selection
+                         (- (list-ref (cdr (gimp-selection-bounds image)) 2)
+                            (list-ref (cdr (gimp-selection-bounds image)) 0))
+                         (- (list-ref (cdr (gimp-selection-bounds image)) 3)
+                            (list-ref (cdr (gimp-selection-bounds image)) 1))
+                         (list-ref (cdr (gimp-selection-bounds image)) 0)
+                         (list-ref (cdr (gimp-selection-bounds image)) 1))
+    ))
+
+    (if (= selection-exists TRUE) (gimp-selection-load user-selection))
 
     (if (= drop-shadow TRUE) (begin                 ; --------- Shadow ---------
         (gimp-image-undo-group-start image)
@@ -176,7 +248,8 @@
         (gimp-drawable-fill white-layer WHITE-FILL)
         (gimp-image-insert-layer image white-layer 0
                                  (car (gimp-image-get-layers image))) ; To end
-        (gimp-image-remove-channel image user-selection)     ; Clean up
+        (if (= selection-exists TRUE)
+            (gimp-image-remove-channel image user-selection)); Clean up
         (gimp-image-remove-channel image bordered-selection) ; channels
     (gimp-image-undo-group-end image)
 
@@ -200,17 +273,14 @@
                                     255 TRUE TRUE ""))
     )
 ;END -----------------------------------------------------------
-  (gimp-displays-flush)
-  (gimp-context-pop)
-) ;let
-) ;define
-
-
+(gimp-displays-flush)
+(gimp-context-pop)
+)) ;let, define
 
 (script-fu-register "script-fu-dx-screenshotv3"
     _"_Screenshot processing 2016..."
     _"Draws border, adds modern shadow and makes wavy crop."
-    "Konstantin Beliakov <Konstantin.Belyakov@devexpress.com>, Vladislav Glagolev <vladislav.glagolev@devexpress.com>"
+    "Vladislav Glagolev <vladislav.glagolev@devexpress.com>, Konstantin Beliakov <Konstantin.Belyakov@devexpress.com>"
     "DevExpress Inc."
     "7/15/2016"
     "RGB* INDEXED* GRAY*"
@@ -225,5 +295,9 @@
     SF-TOGGLE     _"Draw border"                        TRUE
     SF-COLOR      _"Border color"                       "black"
     SF-ADJUSTMENT _"Border opacity (0-100%)"            '(20 0 100 1 10 0 0)
+    SF-TOGGLE     _"Make wavy crop"                     TRUE
+    SF-ADJUSTMENT _"Waves strength (0 - calm, 10 - tsunami)" '(3 0 10 1 0 0)
+	SF-TOGGLE     _"Reverse wave phase"       			FALSE
+
 )
 (script-fu-menu-register "script-fu-dx-screenshotv3" "<Image>/DX")
