7fa036b7b568685afef8335c388f997ac48454d9
Himura Kazuto <glagol15@gmail.com>
2018-04-13 12:49:33 +0300

    Update Install to 2.10.bat

--- Install to 2.10.bat
+++ Install to 2.10.bat
@@ -1,2 +1,4 @@
-COPY dx-screenshot-2016.scm "%APPDATA%\GIMP\2.10\scripts"
+COPY *.scm "%APPDATA%\GIMP\2.10\scripts"
+COPY *.py "%APPDATA%\GIMP\2.10\plug-ins"
+COPY images\*.* "%ProgramFiles%\GIMP 2.10\share\gimp\2.0\scripts\images"
 pause
